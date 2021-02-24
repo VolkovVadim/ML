03f662ee95de7d0eb71570f3db18862a4146ce0e
VolkovVadim <VolkovVadim5@gmail.com>
2021-02-24 21:54:09 +0300

    Expanded section with basic operations

--- PyTorch/Basics/Main.py
+++ PyTorch/Basics/Main.py
@@ -1,44 +1,59 @@
 import torch
 
 
+def print_tensor(x, name=None):
+    if name is not None:
+        print(name)
+
+    print("Type          :", x.type())
+    print("Shape/size    :", x.shape)
+    print("Requires grad :", x.requires_grad)
+    print("Values        :\n", x, "\n")
+
+
 # How to create a tensor
 def tensor_creation():
     print("Tensor creation section")
     # Create empty tensor 3x4
     A = torch.empty(3, 4)
 
-    # Create tensor 3x4 with random values
-    B = torch.rand(3, 4)
+    # Creation a 3x4 tensor with random values from a random uniform distribution
+    # random values from range 0 <= x < 1
+    B1 = torch.rand(3, 4)
+
+    # Creation a 3x4 tensor with random values from a random normal distribution
+    B2 = torch.randn(3, 4)
 
     # Create tensor 3x4 with random values and specific data type
-    C = torch.rand(3, 4, dtype=torch.double)
+    B3 = torch.rand(3, 4, dtype=torch.double)
 
     # Create tensor 3x4 with zeros
-    D = torch.zeros(3, 4)
+    C = torch.zeros(3, 4)
 
     # Create tensor 3x4 with ones
-    E = torch.ones(3, 4)
+    D = torch.ones(3, 4)
 
     # Create tensor from values
-    F1 = torch.tensor(3)
-    F2 = torch.tensor([5, 7, 8.9])
-    F3 = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    E1 = torch.tensor(3)
+    E2 = torch.tensor([5, 7, 8.9])
+    E3 = torch.tensor([[1, 2, 3], [4, 5, 6]])
 
     # Create identity matrix
-    G = torch.zeros(5, 5)
+    F = torch.zeros(5, 5)
     for i in range(5):
-        G[i, i] = 1
+        F[i, i] = 1
 
-    print("Tensor A (empty):\n", A)
-    print("Tensor B (with random values):\n", B)
-    print("Tensor C (with random values and double data type):\n", C)
-    print("Tensor D (with zeros):\n", D)
-    print("Tensor E (with ones):\n", E)
+    print_tensor(A, name="Tensor A (empty)")
+    print_tensor(B1, name="Tensor B1 (with random values from a random uniform distribution)")
+    print_tensor(B2, name="Tensor B2 (with random values from a random normal distribution)")
+    print_tensor(B3, name="Tensor B3 (with random values and double data type)")
+    print_tensor(C, name="Tensor C (with zeros)")
+    print_tensor(D, name="Tensor E (with ones)")
     print("\nTensors created from values")
-    print("Tensor F1:\n", F1)
-    print("Tensor F2:\n", F2)
-    print("Tensor F3:\n", F3, "\n")
-    print("Tensor G:\n", G)
+    print_tensor(E1, name="Tensor E1")
+    print_tensor(E2, name="Tensor E2")
+    print_tensor(E3, name="Tensor E3")
+    print_tensor(F, name="Tensor F")
 
 
 # How to modify a tensor
@@ -51,10 +66,8 @@ def modify_tensor():
     # How to create numpy array from tensor
     x_numpy = x_original.numpy()
 
-    print("X original:\n", x_original)
-    print("shape : ", x_original.size())
-    print("X reshaped:\n", x_reshaped)
-    print("shape :", x_reshaped.size())
+    print_tensor(x_original, name="X original")
+    print_tensor(x_reshaped, name="X reshaped")
     print("X original as numpy array:\n", x_numpy)
 
 
@@ -74,17 +87,17 @@ def tensor_operations():
     y2 = torch.tensor([[1, 1, 1], [1, 1, 1]])
     x2.add_(y2)  # not to be confused with the non-underscore method
 
-    print("Tensor X1:\n", x1)
-    print("Tensor Y1:\n", y1)
-    print("Tensor Z1(sum of tensors X1 and Y1):\n", z1)
-    print("Tensor X2:\n", x2)
+    print_tensor(x1, name="Tensor X1")
+    print_tensor(y1, name="Tensor Y1")
+    print_tensor(z1, name="Tensor Z1(sum of tensors X1 and Y1)")
+    print_tensor(x2, name="Tensor X2")
 
 
 if __name__ == "__main__":
     print("Application started")
     print("Torch version : ", torch.__version__)
-    #tensor_creation()
+    tensor_creation()
     #modify_tensor()
-    tensor_operations()
+    #tensor_operations()
 
     print("Success")
